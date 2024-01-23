5619b63c7f32966b95935b8617cadfff78d22429
alikhokha807 <ali.khokha2000@gmail.com>
2024-01-23 10:28:05 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+def safe_print_list(my_list=None, x=0):
+    if my_list is None:
+        my_list = []
 
-def safe_print_list(my_list=[], x=0):
     total = 0
     for i in range(x):
         try:
@@ -10,6 +12,6 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print("")
-    return (total)
+    return total
  
 
